77fbdcdbd74c1919077bf58615a66f88ba871f42
bilalobe <51460731+bilalobe@users.noreply.github.com>
2023-05-17 20:56:58 +0100

    Create hellospeaker.js

---
+++ hellospeaker.js
@@ -0,0 +1,10 @@
+(function(window) 
+{
+	var speakWord = "Hi";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
